7754bb5cafae58f51876bf06dfb60aa8a9379dac
Thomas van Deukeren <t.b.deukeren@students.uu.nl>
2017-10-26 16:19:08 +0200

    Test

---
+++ Main.hs
@@ -0,0 +1,23 @@
+module Main where
+
+import Graphics.Gloss
+
+
+
+-- De eigenschappen van het venster.
+window :: Display
+window = InWindow "Nice Window" (200, 200) (10, 10)
+
+-- Achtergrondskleur.
+background :: Color
+background = white
+
+-- Een plaatje om te tekenen.
+drawing :: Picture
+drawing = circle 30
+
+
+
+-- display :: Display -> Color -> Picture -> IO()
+main :: IO()
+main = display window background drawing
