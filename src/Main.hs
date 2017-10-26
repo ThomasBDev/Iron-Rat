de6b03d19e0680f4b33cc26bfbec656cf67ff1cf
thobod <t.boddeus@gmail.com>
2017-10-26 16:42:08 +0200

    moveplane, render en gamestate  toegevoegd

--- src/Main.hs
+++ src/Main.hs
@@ -3,16 +3,16 @@ module Main(main) where
 import Graphics.Gloss
 
 window :: Display
-window = InWindow "Nice Window" (800, 1200) (10,10)
+window = InWindow "Nice Window" (1200, 800) (10,10)
 
 background :: Color
 background = white
 
 drawing :: Picture
-drawing = translate 20 20 (circle 30)
+drawing = circle 30
 
 main :: IO ()
-main = display window background drawing
+main = display window background (render (movePlane initialState))
 
 
 data GameState = Game 
@@ -21,4 +21,15 @@ data GameState = Game
 initialState :: GameState
 initialState = Game 
     { planePos = (0.0,0.0), planeSpeed = 0.0 }
+
+movePlane :: GameState -> GameState
+movePlane game = game{planePos = (x,y)}
+    where
+    x = 10
+    y = 300
+
+render :: GameState -> Picture
+render game = pictures [plane]
+    where
+    plane = uncurry translate (planePos game) drawing
 
