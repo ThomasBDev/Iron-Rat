aaefdc7de9906c2ff3df793538ab3dcea95ed6bc
Thomas van Deukeren <t.b.deukeren@students.uu.nl>
2017-11-02 17:14:23 +0100

    Player object aangeroepen mbv. Player module. Witte achtergrond met langwerpige, groene "grond" sprite. Testwaardes voor polygon.

--- src/Game.hs
+++ src/Game.hs
@@ -4,6 +4,7 @@ import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Game
 import Graphics.Gloss.Data.ViewPort
 
+import Player
 import Enemies
 
 
@@ -12,19 +13,20 @@ window :: Display
 window = InWindow "Nice Window" (1000, 600) (10,10)
 
 background :: Color
-background = green
+background = white
 
 fps :: Int
 fps = 60
 
-drawing, drawing1, drawing2 :: Picture
+ground, drawing, drawing1, drawing2 :: Picture
+ground = translate 0 1200 (color green (polygon (rectanglePath 1000 2400)))
 drawing = circle 10
 drawing1 = translate 200 200 (circle 30)
 drawing2 = translate (-400) (-100) (circle 100)
 
 
 pictureList :: [Picture]
-pictureList = [drawing, drawing1, drawing2]
+pictureList = [ground, drawing, drawing1, drawing2, playerSprite startPlayer]
 
 -- testEnemy :: Enemy
 -- testEnemy = testEnemy { status = Alive, position = (0.0, 0.0), enemyType = Plane, sprite = drawing2 }
@@ -40,10 +42,22 @@ data GameState = Game { player :: Player , planeSpeed :: Float , spriteList :: [
 initialState :: GameState
 initialState = Game {player = startPlayer, planeSpeed = 0.0, spriteList = pictureList}
 
-data Player = PlayerInfo {playerPos :: (Float, Float), health :: Int, picture :: Picture}
+
+
+linePath :: Path
+linePath = [(0, 0), (100, 0), (100, 100)]
+
+trianglePath :: Path
+trianglePath = [(0.0, 0.0), (100, 100), (-200, 200)]
+
+lijn = line trianglePath
+driehoek = polygon trianglePath
+vierkant = polygon (rectanglePath 100 100)
 
 startPlayer :: Player
-startPlayer = PlayerInfo {playerPos = (0.0,0.0), health = 100, picture = circle 30}
+startPlayer = PlayerInfo {playerPos = (0.0,0.0), health = 100, playerSprite = color blue vierkant}
+
+
 
 update :: Float ->  GameState -> GameState
 update _ game = game
